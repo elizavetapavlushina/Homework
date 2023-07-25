64b281fdf41d70e4248c9a2c82eb87a526b37a27
elizavetapavlushina <elpavlushina@mail.ru>
2023-07-25 22:09:50 +0700

    Local exchange

---
+++ sockets/tcp/client.c
@@ -0,0 +1,87 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define ADDRESS "/tmp/stream_serv"
+#define SIZE_BUFF 256
+
+bool get_message(char * buffer) {
+
+	int i = 0;
+	int symbol;
+	while((symbol = getchar()) != '\n') {
+
+		if (i < SIZE_BUFF - 1){
+			buffer[i] = symbol;
+			i++;
+		}
+		else return false;
+		
+	}
+	buffer[i] = '\0';
+
+	return true;
+}
+
+int main() {
+
+	struct sockaddr_un server;
+	int sock_fd;
+
+	if((sock_fd = socket(AF_LOCAL, SOCK_STREAM, 0)) == -1) {
+
+		perror("client: socket error");
+		exit(1);
+	}
+	
+	server.sun_family = AF_LOCAL;
+	strcpy(server.sun_path, ADDRESS);
+
+	int addrlen = 0;
+	addrlen = sizeof(server.sun_family) + strlen(server.sun_path);
+
+	if(connect(sock_fd, (struct sockaddr *)&server, addrlen) == -1) {
+
+		perror("client: connect error");
+		exit(1);
+	}
+	
+	char message[SIZE_BUFF] = "";
+
+	printf("Enter the massage:\n");
+	if(!(get_message(message))) {
+
+		printf("server: message is full\n");
+		exit(1);
+
+	} else {
+
+		printf("The message sended\n");
+	}
+
+	if(send(sock_fd, &message, SIZE_BUFF, 0) == -1) {
+
+		perror("client: send message error");
+		exit(1);
+	}
+
+	if(recv(sock_fd, &message, SIZE_BUFF, 0) == -1) {
+
+		perror("client: receive message error");
+		exit(1);
+
+	} else {
+
+		printf("The message received\n");
+	}
+
+	printf("%s\n", message);
+
+	close(sock_fd);
+	exit(EXIT_SUCCESS);
+	
+}
---
+++ sockets/tcp/server.c
@@ -0,0 +1,81 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define ADDRESS "/tmp/stream_serv"
+#define SIZE_BUFF 256
+
+int main() {
+
+	struct sockaddr_un server, client;
+	int sock_fd;
+
+	if((sock_fd = socket(AF_LOCAL, SOCK_STREAM, 0)) == -1) {
+
+		perror("server: socket error");
+		exit(1);
+	}
+
+	server.sun_family = AF_LOCAL;
+	strcpy(server.sun_path, ADDRESS);
+	unlink (ADDRESS);
+
+	int addrlen = 0;
+	addrlen = sizeof(server.sun_family) + strlen(server.sun_path);
+	
+	if(bind(sock_fd, (struct sockaddr *)&server, addrlen) == -1) {
+
+		perror("server: bind error");
+		exit(1);
+	}
+	
+	if(listen(sock_fd, 5) == -1) {
+
+		perror("server: listen error");
+		exit(1);
+	}
+	
+
+	int new_s_fd;
+	int client_ad_len = 0;
+	client_ad_len = sizeof(client.sun_family) + strlen(client.sun_path);
+
+	if((new_s_fd = accept(sock_fd, (struct sockaddr *)&client, &client_ad_len)) == -1) {
+
+		perror("server: accept error");
+		exit(1);
+	}
+	
+	char message[SIZE_BUFF] = "";
+
+	if(recv(new_s_fd, &message, SIZE_BUFF, 0) == -1) {
+
+		perror("server: receive message error");
+		exit(1);
+
+	} else {
+
+		printf("The message received\n");
+	}
+	printf("%s\n", message);
+
+	char serv_mes[SIZE_BUFF]= "Hello!";
+	if(send(new_s_fd, &serv_mes, SIZE_BUFF, 0) == -1) {
+
+		perror("server: send message error");
+		exit(1);
+	} else {
+
+		printf("The message sended\n");
+	}
+	
+	close(new_s_fd);
+	close(sock_fd);
+
+	exit(EXIT_SUCCESS);
+
+}
